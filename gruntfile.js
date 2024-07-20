41f5def02fb3514067d5a04cf2b8558e341c352f
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-07-20 01:09:03 -0300

    Configurado Plugin do Grunt LESS
    - Ajustado o arquivo ".gitignore";
    - Configurado o Plugin;
    - Adicionado Customização ao "main.less";
    - Executado Grunt;

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            }
+        }
     })
+
+
+grunt.loadNpmTasks('grunt-contrib-less');
+
+grunt.registerTask('default', ['less']);
 }
+
